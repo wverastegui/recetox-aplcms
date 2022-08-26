37727ee70bf33721d3c2713b82a6b8d4a1f6ec56
wverastegui <wudmir@gmail.com>
2022-08-26 11:15:36 +0000

    Updating test-unsupervised

--- tests/testthat/test-unsupervised.R
+++ tests/testthat/test-unsupervised.R
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 patrick::with_parameters_test_that(
   "basic unsupervised test",
   {
-    ######## Handle clusters ###########
     # CRAN limits the number of cores available to packages to 2
     # source https://stackoverflow.com/questions/50571325/r-cran-check-fail-when-using-parallel-functions#50571533
     chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
@@ -19,7 +17,6 @@ patrick::with_parameters_test_that(
       cluster <- parallel::makeCluster(cluster)
       on.exit(parallel::stopCluster(cluster))
     }
-    #######
     
     # set actual and expected entries
     testdata <- file.path("..", "testdata")
@@ -46,8 +43,8 @@ patrick::with_parameters_test_that(
       sigma_ratio_lim = sigma_ratio_lim,
       component_eliminate = 0.01,
       moment_power = 1,
-      # mz_tol_relative = align_mz_tol,
-      # rt_tol_relative = align_rt_tol,
+      align_mz_tol = mz_tol_relative,
+      align_rt_tol = rt_tol_relative,
       max_align_mz_diff = max_align_mz_diff,
       recover_mz_range = recover_mz_range,
       recover_rt_range = recover_rt_range,
@@ -56,19 +53,19 @@ patrick::with_parameters_test_that(
       intensity_weighted = intensity_weighted,
       cluster = cluster
     )
-    
+
     aligned_feature_sample_table_expected <- arrow::read_parquet(file.path(testdata, "recovered", "aligned_feature_sample_table.parquet"))
     recovered_feature_sample_table_expected <- arrow::read_parquet(file.path(testdata, "recovered", "recovered_feature_sample_table.parquet"))
 
 
     expect_equal(actual$aligned_feature_sample_table, aligned_feature_sample_table_expected)
     expect_equal(actual$recovered_feature_sample_table, recovered_feature_sample_table_expected)
-  
+  browser()
   },
   patrick::cases(
     RCX_shortened = list(
       files = c("RCX_06_shortened", "RCX_07_shortened", "RCX_08_shortened"),
-      mz_tol = 1e-05,
+      mz_tol = 1e-06,
       min_pres = 0.5,
       min_run = 12,
       intensity_weighted = FALSE,
@@ -80,36 +77,9 @@ patrick::with_parameters_test_that(
       use_observed_range = TRUE,
       min_bandwidth = NA,
       max_bandwidth = NA,
-      recover_min_count = 3
-      # align_mz_tol = NA,
-      # align_rt_tol = NA
+      recover_min_count = 3,
+      mz_tol_relative = NA,
+      rt_tol_relative = NA
     )
   )
 ) 
-
-
-
-
-# test_that("basic unsupervised test", {
-#   test_files <- c('../testdata/mbr_test0.mzml',
-#                   '../testdata/mbr_test1.mzml',
-#                   '../testdata/mbr_test2.mzml')
-  
-#   expected <- arrow::read_parquet('../testdata/unsupervised_recovered_feature_sample_table.parquet')
-  
-#   # CRAN limits the number of cores available to packages to 2
-#   # source https://stackoverflow.com/questions/50571325/r-cran-check-fail-when-using-parallel-functions#50571533
-#   chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
-  
-#   if (nzchar(chk) && chk == "TRUE") {
-#     # use 2 cores in CRAN/Travis/AppVeyor
-#     num_workers <- 2L
-#   } else {
-#     # use all cores in devtools::test()
-#     num_workers <- parallel::detectCores()
-#   }
-  
-#   result <- unsupervised(test_files, cluster = num_workers)
-
-#   expect_equal(result$recovered_feature_sample_table, expected)
-# })
